4093651f37f70ceb1b79a489543fa28087694f86
Mariusz Kabała <mariusz@kabala.waw.pl>
2020-11-08 10:02:42 +0100

    feat: add menu icons

--- packages/lights/src/components/Menu/index.tsx
+++ packages/lights/src/components/Menu/index.tsx
@@ -3,8 +3,33 @@ import { useSelector } from "react-redux";
 import cx from "classnames";
 import { Link, useLocation } from "react-router-dom";
 import { FaAngleDoubleRight } from "react-icons/fa";
+import { AiFillBulb } from "react-icons/ai";
+import { MdKitchen, MdTv } from "react-icons/md";
+import { BiBath, BiBed, BiDoorOpen } from "react-icons/bi";
 
 import styles from "./styles.module.scss";
+
+const getRoomIcon = (id: string) => {
+  switch (id) {
+    case "kitchen":
+      return <MdKitchen />;
+
+    case "living-room":
+      return <MdTv />;
+
+    case "bathroom":
+      return <BiBath />;
+
+    case "bedroom":
+      return <BiBed />;
+
+    case "corridor":
+      return <BiDoorOpen />;
+
+    default:
+      return null;
+  }
+};
 
 export const Menu: FC<{}> = () => {
   const rooms = useSelector((state) => state.lightsApp?.rooms);
@@ -24,8 +49,13 @@ export const Menu: FC<{}> = () => {
                 [styles.active]: location.pathname === `/`,
               })}
             >
+              <b className={styles.icon}>
+                <AiFillBulb />
+              </b>
               <h3>Overview</h3>
-              <FaAngleDoubleRight />
+              <b className={styles.more}>
+                <FaAngleDoubleRight />
+              </b>
             </li>
           </Link>
           {rooms.map((room) => (
@@ -35,11 +65,16 @@ export const Menu: FC<{}> = () => {
                   [styles.active]: location.pathname === `/${room.id}`,
                 })}
               >
-                <h3>{room.name}</h3>
-                <p>
-                  <strong>{room.groups.length}</strong> in the room
-                </p>
-                <FaAngleDoubleRight />
+                <b className={styles.icon}>{getRoomIcon(room.id)}</b>
+                <div>
+                  <h3>{room.name}</h3>
+                  <p>
+                    <strong>{room.groups.length}</strong> in the room
+                  </p>
+                </div>
+                <b className={styles.more}>
+                  <FaAngleDoubleRight />
+                </b>
               </li>
             </Link>
           ))}
